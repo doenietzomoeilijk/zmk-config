c94e15edb018d75098eb598be77347dbf03ae824
Max Roeleveld <m.g.roeleveld@rug.nl>
2024-02-18 21:21:58 +0100

    Improve build script.

--- scripts/do-build.sh
+++ scripts/do-build.sh
@@ -4,20 +4,31 @@ BOARD=lily58
 NOW=$(date +"%Y%m%d-%H%M%S")
 REV=$(git --git-dir=/zmk-config/.git rev-parse --short HEAD)
 OUTDIR="/zmk-config/firmware/${NOW}-${REV}"
+LOG="${OUTDIR}/build.log"
 ZMKDIR=/zmk-config/zmk
 mkdir ${OUTDIR}
 
 git config --global --add safe.directory '*'
 cd ${ZMKDIR}
-west init -l app/
-west update
+
+echo "Writing to logfile ${LOG}..."
+echo "**********************************************************************" >> $LOG
+echo "West init/update" >> $LOG
+west init -l app/ >> $LOG
+west update >> $LOG
+
+echo "**********************************************************************" >> $LOG
+echo "Builds..." >> $LOG
 
 for side in left right; do 
+    echo "\n${side} side\n" >> $LOG
+    echo "**********************************************************************" >> $LOG
     west build -b nice_nano_v2 \
         -s "${ZMKDIR}/app" \
         -d "build/${BOARD}/${side}" -- \
         -DZMK_CONFIG=/zmk-config/config \
-        -DSHIELD="${BOARD}_${side}"
+        -DSHIELD="${BOARD}_${side}" >> $LOG
+
     cp "${ZMKDIR}/build/${BOARD}/${side}/zephyr/zmk.uf2" "${OUTDIR}/${BOARD}-${side}-${NOW}-${REV}.uf2"
 done
 
