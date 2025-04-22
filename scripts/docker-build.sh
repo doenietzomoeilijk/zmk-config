6815c0da6cac4520f08ab9d3af949fb1697252a1
Max Roeleveld <m.g.roeleveld@rug.nl>
2025-04-22 11:31:49 +0200

    Switch from Docker to Podman.

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-docker run --rm \
+podman run --rm \
     -v "$(git rev-parse --show-toplevel):/zmk-config" \
     zmkfirmware/zmk-build-arm:3.5-branch \
     /zmk-config/scripts/do-build.sh
